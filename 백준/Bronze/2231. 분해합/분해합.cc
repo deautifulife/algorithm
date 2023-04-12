f10de3cc70526d11b59233a20fd52cacff1ff509
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-04-12 09:52:44 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    int ans=0;
+    cin>>n;
+    for(int i=1; i<n; i++) {
+        int result =0;;
+        int num = i;
+        result = num;
+        while(1) {
+            if(num<10){
+                result+=num;
+                break;
+            }
+            result+=num%10;
+            num=num/10;
+        }
+
+        if(result==n){
+            ans = i;
+            break;
+        }
+    }
+    cout<<ans;
+
+    return 0;
+
+}

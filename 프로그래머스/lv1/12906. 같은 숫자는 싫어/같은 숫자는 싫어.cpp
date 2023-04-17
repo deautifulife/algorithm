4cc4483f4f13c3d315311f4f388294e422f08859
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-04-17 16:55:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 124.60 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -11,11 +11,13 @@ vector<int> solution(vector<int> arr)
     stack<int> s;
     
     for(int i= arr.size()-1; i>=0; i--){
+        //스택이 안비워져있고 스택 top이 현대 인덱스와 같은 값이라면 팝하고 마지막에 푸쉬해줌
         while(!s.empty() && s.top() == arr[i]){
             s.pop();
         }
         s.push(arr[i]);
     }
+    //스택이 빌때까지 top값을 정답벡터에 넣고 팝을 반복
     while(!s.empty()){
         answer.push_back(s.top());
         s.pop();

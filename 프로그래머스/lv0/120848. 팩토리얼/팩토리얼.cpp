a46d72378f3f3e67df1f83408e1f143e6740e296
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-04-21 19:39:45 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int num=1;
+    while(num<=n){
+        answer++;
+        num = num * answer;
+    }
+    return answer-1;
+}

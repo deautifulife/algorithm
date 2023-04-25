875ae4dc5f8bd6f59bc22ccb0e876d0efe3d7ccf
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-04-25 18:02:13 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(int i=0; i<my_string.length(); i++){
+        if(my_string[i] >= 'a')
+            answer.push_back(my_string[i]-32);
+        else
+            answer.push_back(my_string[i]+32);
+    }
+    return answer;
+}

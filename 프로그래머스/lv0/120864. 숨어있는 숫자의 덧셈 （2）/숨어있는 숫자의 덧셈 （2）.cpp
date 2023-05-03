4e54fab620646dcf37f2c52eb3c81cdb4327b529
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-05-03 22:08:17 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    int n=0;
+    for(int i=0; i<my_string.length(); i++){
+        if(my_string[i] >= '0' && my_string[i] <= '9'){
+            n = n * 10 + my_string[i]-'0';
+        }
+        else{
+            answer+= n;
+            n=0;
+        }        
+    }
+    answer+=n;
+    return answer;
+}

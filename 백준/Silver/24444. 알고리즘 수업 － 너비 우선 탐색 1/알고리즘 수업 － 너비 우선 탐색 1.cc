bec3b05e14b8d13b6889486476b391fee489f008
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-08-09 14:28:33 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 88 ms, Memory: 9944 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> map;
+vector <int> visited;
+vector <int> ans;
+int cnt;
+
+void bfs(int r){
+	visited[r] = 1;
+	queue<int> q;
+	q.push(r);
+	ans[r] = ++cnt; //처음 방문한 곳 카운트
+	while(!q.empty()){
+		int dx = q.front();
+		q.pop();
+		for(int i=0; i<map[dx].size(); i++){
+			int nx = map[dx][i];
+			if(visited[nx] == 0){
+				q.push(nx);
+				visited[nx] = 1;
+				ans[nx] = ++cnt;
+			}
+		}
+	}
+}
+
+
+int main(void) {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int n,m,r;
+	cin>>n>>m>>r;
+
+	map.resize(n+1);
+	visited.resize(n+1);
+	ans.resize(n+1);
+
+	for(int i=0; i<m; i++)	{
+		int u,v;
+		cin>>u>>v;
+		//방향이 없으므로 양방향
+		map[u].push_back(v);
+		map[v].push_back(u);
+
+	}
+
+//오름차순 정리
+	for(int i=1; i<=n; i++)	{
+		sort(map[i].begin(), map[i].end());
+	}
+
+	bfs(r);
+
+	for(int i=1; i<=n; i++){
+		cout<<ans[i]<<'\n';
+	}
+
+}

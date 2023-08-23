5c9c38c6c2d6a0f5ec872322f32d84d4bdadb9be
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-08-23 15:45:26 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	int Number;
+
+	int result;
+
+	int A,B,C,D;
+
+	int count = 0;
+
+	
+
+	scanf("%d",&Number);
+
+	result = Number;
+
+	
+
+	while(1)
+
+	{
+
+		A = Number / 10;
+
+		B = Number % 10;
+
+		C = (A+B) % 10;
+
+		D = (B*10) + C;
+
+		Number = D;
+
+		count++;
+
+		if(D==result) break;
+
+	}
+
+	printf("%d",count);
+
+}

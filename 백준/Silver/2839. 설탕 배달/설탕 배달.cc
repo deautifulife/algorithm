cb7bee31cbf01328cf64ce51733cf71af1c8c03a
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-08-28 13:45:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-int dp[5001];
 
 int main(){
 
@@ -13,25 +12,18 @@ int main(){
 	cout.tie(NULL);
 	
 	int n;
+	int ans = 0;
 	cin >> n;
-	dp[3] = dp[5] = 1;
-
-	for (int i = 6; i <= n; i++) {
-		if (dp[i - 3])
-			dp[i] = dp[i - 3] + 1;
-
-		if (dp[i - 5]) {
-			if (dp[i] == 0)
-				dp[i] = dp[i - 5] + 1;
-			else
-				dp[i] = min(dp[i], dp[i - 5] + 1);
+	while (n >= 0) {
+		if (n % 5 == 0) {
+			ans += (n / 5);
+			cout << ans;
+			return 0;
 		}
+		n -= 3;
+		ans++;
 	}
-
-	if (dp[n] == 0)
-		cout << "-1";
-	else
-		cout << dp[n];
+	cout << -1;
 	
 
 	return 0;

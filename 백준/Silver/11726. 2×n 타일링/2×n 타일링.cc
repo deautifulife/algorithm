5dc08441c6f34a0c582ea5b58be6f4878723fe83
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-08-31 22:59:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+//#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+long long dp[1001];
+
+int main(){
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}

a106e0a143012869bc05b654f8a282f9212e2c73
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-11-03 21:06:41 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2736 ms, Memory: 3344 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -5,22 +5,22 @@
 using namespace std;
 
 #define MAX 10001
-int n,m;
+int n, m;
 vector<int> map[MAX];
 bool visited[MAX];
 queue<int> q;
-vector<pair<int,int>> v; //COM num, 해킹가능한 COM tn
+vector<pair<int, int>> v; //COM num, 해킹가능한 COM 수
 int hacked = 1;
 
-void BFS(int v){
-	visited[v] = true;
-	q.push(v);
-	while(!q.empty()){
-		v = q.front();
+void BFS(int pc) {
+	visited[pc] = true;
+	q.push(pc);
+	while (!q.empty()) {
+		pc = q.front();
 		q.pop();
-		for(int i=0; i<map[v].size(); i++){
-			int next_v = map[v][i];
-			if(visited[next_v] == 0){
+		for (int i = 0; i < map[pc].size(); i++) {
+			int next_v = map[pc][i];
+			if (visited[next_v] == 0) {
 				visited[next_v] = true;
 				q.push(next_v);
 				hacked++;
@@ -31,38 +31,38 @@ void BFS(int v){
 
 
 void resetVisited() {
-	for(int i=0; i<=n; i++)
+	for (int i = 0; i <= n; i++)
 		visited[i] = 0;
 }
 
 int main(void) {
-	
+
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin>>n>>m;
-	for(int i=0; i<m; i++){
-		int s,e;
-		cin>>s>>e;
-		map[e].push_back(s);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int s, e;
+		cin >> s >> e; //s는 e를 신뢰함
+		map[e].push_back(s); //e를 해킹하면 해킹할 수 있는 컴퓨터 목록에 s를 추가
 	}
 
-	for(int i=1; i<=n; i++){
+	for (int i = 1; i <= n; i++) {
 		BFS(i);
 		resetVisited();
-		v.push_back(make_pair(i,hacked));
+		v.push_back(make_pair(i, hacked));
 		hacked = 1;
 	}
 
 	int maxHack = -1;
-	for(int i=0; i<v.size(); i++){
-		if(v[i].second > maxHack)
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i].second > maxHack)
 			maxHack = v[i].second;
 	}
 
-	for(int i=0; i< v.size(); i++){
-		if(v[i].second == maxHack)
-			cout<<v[i].first<<" ";
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i].second == maxHack)
+			cout << v[i].first << " ";
 	}
 }

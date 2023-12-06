c3d4e7d80a5f3d9a2e7dea509c286b1d7cba5e5d
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-12-06 13:38:10 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.cc
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.cc
@@ -1,75 +1,66 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
-int main(){
+int M;
+int stock[14];
+int Jmoney, Smoney, Jstock, Sstock;
+int upcnt, downcnt;
+int Jret, Sret;
 
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+void BMP(int day) {
+	if (Jmoney >= stock[day]) {
+		Jstock += Jmoney / stock[day];
+		Jmoney = Jmoney % stock[day];
+	}
+}
 
-    int n;
-    cin>>n; //시작 현금
+void TIMING(int day) {
+	if (day > 0 && stock[day] > stock[day - 1]) {
+		upcnt++;
+		downcnt = 0;
+	}
+	else if (day > 0 && stock[day] < stock[day - 1]) {
+		downcnt++;
+		upcnt = 0;
+	}
 
-    vector<int> v; //날별 주식 가격
-    for(int i=0; i<14; i++){
-        int inp;
-        cin>>inp;
-        v.push_back(inp);
-    }
+	//전량매도
+	if (upcnt >= 3) {
+		Smoney += Sstock * stock[day];
+		Sstock = 0;
+	}
+	//전량매수
+	if (downcnt >= 3) {
+		if (Smoney >= stock[day]) {
+			Sstock += Smoney / stock[day];
+			Smoney = Smoney % stock[day];
+		}
+	}
+}
+int main(void) {
 
-    int jun =0, sung =0;
-    int _n = n;
-    
-    //준현이의 매수
-    for(auto it : v){
-        if(it <= _n){
-            jun = _n / it;
-            _n = _n % it;
-            break;
-        }
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> M;
+	Jmoney =M, Smoney=M;
+	for (int i = 0; i < 14; i++) {
+		cin >> stock[i];
+		BMP(i);
+		TIMING(i);
+	}
 
-    jun = _n + (jun * v[13]); //마지막날 매도
+	Jret = Jmoney + stock[13] * Jstock;
+	Sret = Smoney + stock[13] * Sstock;
 
-    int cnt =0;
-    int buy_idx = 999999; // 왜한거지?
-    for(int i=1; i<v.size(); i++){
-        //하락하는지 보는 것
-        if(v[i] < v[i-1])
-            cnt++;
-        else cnt = 0;
-        //3일 연속 하락 = 매수시점
-        if(cnt >=3){
-            sung += n / v[i];
-            n = n % v[i];
-            buy_idx = i; //매수일 기억
-        }
-    }
-    cnt = 0;
-    bool flag = false;
-    for(int i = buy_idx+1; i<v.size(); i++){
-        //상승하는지 보는 것
-        if(v[i] > v[i-1])
-            cnt++;
-        else cnt =0;
-        //3일 연속 상승 == 매도 시점
-        if(cnt >= 3){
-            flag = true; //매도했음을 기록
-            sung = sung * v[i];
-            break;
-        }
-    }
+	if (Jret > Sret)
+		cout << "BNP";
+	else if (Jret < Sret)
+		cout << "TIMING";
+	else if (Jret == Sret)
+		cout << "SAMESAME";
 
-    if(!flag)
-        sung = n + (sung * v[13]); //매도 안했었으면 마지막날 매도
-        if(sung > jun)
-            cout<<"TIMING";
-        else if(sung < jun)
-            cout<<"BNP";
-        else
-            cout<<"SAMESAME";
 	return 0;
 }

9cd8aad6e1a04c0857a3dfd43c5e1dd3472b4ab0
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2023-12-07 15:23:00 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
@@ -1,50 +1,67 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
-int n, student, g, y;
-bool arr[101];
+int N,S;
+int swit[101];
 
-int main(){
-
-ios::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
-
-cin>>n;
-for(int i=1; i<=n; i++){
-    cin>>arr[i]; //스위치의 상태 입력 받기
+void trans(int num) {
+	if (swit[num] == 0)
+		swit[num] = 1;
+	else if (swit[num] == 1)
+		swit[num] = 0;
 }
 
-cin>>student; //학생 수 입력 받기
-for(int k=0; k<student; k++){
-    cin>>g>>y; //학생의 성별, 스위치 번호
-
-    if(g==1) { //남학생의 경우
-        for(int i=1; i<=n; i++){
-            if(i%y==0) //배수일때
-                arr[i]= !arr[i]; //스위치 전환
-        }
-    }
-
-    else{ //여학생
-        arr[y] = !arr[y]; // 우선 자기 스위치 상태 ㅏㅂ꿈
-        for(int i=1; arr[y+i] == arr[y-i]; i++){ // 대칭이면 반복
-            if(y+i>n || y-i<1) //인덱스 벗어나면 멈춤
-                break;
-            
-            arr[y+i] = !arr[y+i]; //스위치 전환
-            arr[y-i] = !arr[y-i]; //스위치 전환
-        }
-    }
+void command(int sex, int num) {
+	if (sex == 1) {
+		for (int i = num; i <= N; i += num) {
+			trans(i);
+		}
+	}
+	else if (sex == 2) {
+		trans(num);
+		int front = num - 1;
+		int back = num + 1;
+		while (1) {
+			if (front <1 || back >N)
+				break;
+			if (swit[front] == swit[back]) {
+				trans(front);
+				trans(back);
+				front--;
+				back++;
+				
+			}
+			else
+				break;
+		}
+	}
 }
 
-for(int i=1; i<=n; i++){
-    cout<<arr[i]<<" ";
-    if(i%20==0) //20개의 숫자를 출력했다면 줄바꾸기
-        cout<<'\n';
-}
-return 0;
+int main(void) {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> swit[i];
+	}
+	cin >> S;
+	for (int i = 0; i < S; i++) {
+		int sex, num;
+		cin >> sex >> num;
+		command(sex, num);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		cout << swit[i] << ' ';
+		if (i % 20 == 0) {
+			cout << '\n';
+		}
+
+	}
+
+	return 0;
 }

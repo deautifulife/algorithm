bcd63cc2d640c8d0899ad5f01b23ac995bfe0aab
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2024-01-11 17:46:40 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,55 +1,62 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-int n,m;
 int map[101][101];
-int dist[101][101];
 bool visited[101][101];
+int dist[101][101];
+int n, m;
 
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int dx[4] = {1,0,-1,0};
 
-void bfs(int row, int col){
-    queue<pair<int,int>> q;
-    q.push(make_pair(row,col));
-    visited[row][col] = true;
+// 큐
+void BFS(int row, int col) {
+    queue<pair<int, int>> q;
+    q.push(make_pair(row, col));
+    visited[row][col] = 1;
     dist[row][col]++;
-    while(!q.empty()){
-        int front_x = q.front().first;
-        int front_y = q.front().second;
+
+    while (!q.empty()) {
+        int cy = q.front().first;
+        int cx = q.front().second;
         q.pop();
 
-        for(int i=0; i<4; i++){
-            int next_x = front_x + dx[i];
-            int next_y = front_y + dy[i];
-            // if(next_x < 0 || next_x >= n || next_y < 0 || next_y >= n)
-            //     continue;
-            // if(visited[next_x][next_y] == false && map[next_x][next_y] == 1){
-                if(0<= next_x && next_x < n && 0<= next_y && next_y < m && visited[next_x][next_y] == false && map[next_x][next_y] == 1) { // 다음 좌표가 방문한 적 없으면서 1이면
-                q.push(make_pair(next_x,next_y));
-                visited[next_x][next_y] = true;
-                dist[next_x][next_y] = dist[front_x][front_y] + 1;
+        for (int i = 0; i < 4; i++) {
+            int ny = cy + dy[i];
+            int nx = cx + dx[i];
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+                continue;
+            }
+            if (visited[ny][nx] == 0 && map[ny][nx] == 1) {
+                q.push(make_pair(ny, nx));
+                visited[ny][nx] = 1;
+                dist[ny][nx] = dist[cy][cx] + 1;
             }
         }
     }
 }
 
+
+
 int main() {
+
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin>>n>>m;
-    for(int i=0; i<n; i++){
+    cin >> n >> m;
+    for (int y = 0; y < n; y++) {
         string row;
-        cin>>row;
-        for(int j=0; j<m; j++){
-            map[i][j] = row[j]-'0';
+        cin >> row;
+        for (int x = 0; x < m; x++) {
+            map[y][x] = row[x] - '0';
         }
     }
 
-    bfs(0,0);
-    cout<<dist[n-1][m-1];
+    BFS(0, 0);
+
+    cout << dist[n-1][m-1];
 }

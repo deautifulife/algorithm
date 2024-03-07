50d2bba698bacdb10b86149ead48b97787d8811a
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2024-03-07 15:52:50 +0900

    [Silver IV] Title: 카드 놓기, Time: 0 ms, Memory: 2428 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<string> v;
+set<string> s;
+
+
+
+int main(){
+
+    int n,k;
+    cin>>n>>k;
+
+    for(int i=0; i<n; i++){
+        string num;
+        cin>>num;
+        v.push_back(num);
+    }
+
+    if(k==2){
+        for(int i=0; i<v.size(); i++){
+            for(int j=0; j<v.size(); j++){
+                if(i==j)
+                    continue;
+
+                string temp = "";
+                temp += v[i];
+                temp += v[j];
+                s.insert(temp);
+            }
+        }
+    }
+
+    else if(k==3){
+        for(int i=0; i<v.size(); i++){
+            for(int j=0; j<v.size(); j++){
+                for(int k=0; k<v.size(); k++){
+                    if(i==j || i==k || j==k)
+                        continue;
+                    string temp = "";
+                    temp += v[i];
+                    temp += v[j];
+                    temp += v[k];
+                    s.insert(temp);
+                }
+            }
+        }
+    }
+
+    else if(k==4){
+        for(int i=0; i<v.size(); i++){
+            for(int j=0; j<v.size(); j++){
+                for(int k=0; k<v.size(); k++){
+                    for(int l=0; l<v.size(); l++){
+                        if(i==j || i==k || i==l || j==k || j==l || k==l)
+                            continue;
+                        string temp = "";
+                        temp += v[i];
+                        temp += v[j];
+                        temp += v[k];
+                        temp += v[l];
+                        s.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<s.size();
+    return 0;
+}

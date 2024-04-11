69f73b4eb02a5d582f0500de565cef6bf24012ee
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2024-04-11 17:28:25 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -12,29 +12,28 @@ int main() {
     cout.tie(NULL);
 
     int n;
-    cin>>n;
+    cin >> n;
 
     vector<int> A(n);
 
-    for(int i=0; i<n; i++) {
-        cin>>A[i]; //걸리는 시간
+    for (int i = 0; i < n; i++) {
+        cin >> A[i]; //걸리는 시간
     }
 
     sort(A.begin(), A.end()); //걸리는 시간 빠른 순으로 정렬
 
     //계산
     int answer = 0;
-    int time;
-    for(int i=0; i<n; i++) {
-        time = A[i];
-        if(i>0) {
-            A[i] += A[i-1];
+    int time = A[0];
+    for (int i = 0; i < n; i++) {
+        if (i > 0) {
+            A[i] += A[i - 1];
             time = A[i];
         }
-            
+
         answer += time;
     }
 
-    cout<<answer;
+    cout << answer;
 
 }

cc073769dbf7a245938a7d5ae0ceaf5f75fe453d
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2024-04-25 14:15:15 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+bool visited[9];
+int sequence[9];
+
+void makesequence(int depth) {
+    if(depth == m){
+        for (int index = 0; index < m; index++) {
+            cout << sequence[index] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    else {
+        for (int index = 1; index <= n; index++) {
+            if (visited[index] == false && index > sequence[depth-1]) {
+                visited[index] = true;
+                sequence[depth] = index; //수열의 depth자리에 index
+                makesequence(depth + 1);
+                visited[index] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    makesequence(0);
+
+    return 0;
+}

e5b09b5db54e7023f58b6e6393da613efc5be137
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2024-07-06 16:32:02 +0900

    [Silver V] Title: 부분 문자열, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.cc
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.cc
@@ -3,32 +3,30 @@
 
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    string s,t;
-    while(cin>>s>>t){
-        int sIdx = 0;
-        bool isTrue = false;
-        for(int i=0; i<t.length(); i++){
-            //문자열 찾기
-            if(s[sIdx] == t[i])
-                sIdx++;
-            
-            //다 찾으면
-            if(sIdx == s.length()){
-                isTrue=true;
-                break;
-            }
-        }
-        if(isTrue)
-            cout<<"Yes"<<'\n';
-        else
-            cout<<"No"<<'\n';
-    }
+	string s, t;
+	while(cin>>s>>t){
+		int cnt=0;
+		bool isTrue = false;
+		for(int i=0; i<t.length(); i++){
+			if(t[i] == s[cnt]){
+				cnt++;
+			}
+			if(cnt == s.length()){
+				isTrue = true;
+				break;
+			}
+		}
+		
+		if(isTrue)
+			cout<<"Yes"<<'\n';
+		else
+			cout<<"No"<<'\n';
+	}
 
 	return 0;
 }

96ced1d74974734904425d7160794c9e6b0d7d9b
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2024-09-03 14:55:43 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 52 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -4,38 +4,43 @@
 
 using namespace std;
 
+int n, k;
+vector<int> ret;
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    int n,k;
-    vector<int> ans;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>n>>k;
-    queue<int> q;
-    for(int i=1; i<=n; i++){
-        q.push(i);
-    }
+	cin >> n >> k;
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
 
-    while(!q.empty()){
-        for(int i=1; i<=k; i++){
-            if(i==k){
-                ans.push_back(q.front());
-                q.pop();
-            }
-            else{
-                q.push(q.front());
-                q.pop();
-            }
-        }
-    }
-    cout<<"<";
-    for(int i=0; i<ans.size(); i++){
-        if(i==ans.size()-1){
-            cout<<ans[i]<<">";
-        }
-        else
-            cout<<ans[i]<<','<<" ";
-    }
+	while (!q.empty()) {
+		for (int i = 0; i < k; i++) {
+			if (i == k-1) {
+				ret.push_back(q.front());
+				q.pop();
+			}
+			else {
+				q.push(q.front());
+				q.pop();
+			}
+		}
+	}
+
+	cout << "<";
+	for (int i = 0; i < ret.size(); i++) {
+		if (i == ret.size() - 1) {
+			cout << ret[i];
+			break;
+		}
+		cout << ret[i] << ", ";
+	}
+	cout << ">";
+
+	return 0;
 }

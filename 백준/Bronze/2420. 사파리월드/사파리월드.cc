3cb1cf29ba14fe2d4c2df35d4403a0c0ca8cae2a
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2025-08-06 23:14:58 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>  // abs 사용을 위한 헤더
+
+using namespace std;
+
+int main(void) {
+    long long n, m;
+    cin >> n >> m;
+    cout << abs(n - m) <<'\n';
+    return 0;
+}

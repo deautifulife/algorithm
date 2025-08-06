662de6c0a102e348ef2ae9d23f43fa9668978c91
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2025-08-06 23:45:07 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <vector>
+#include <string>
+
+using namespace std;
+
+string input;
+int i;
+
+
+int main(void) {
+
+    cin>>input>>i;
+
+    cout<<input[i-1];
+
+    return 0;
+}

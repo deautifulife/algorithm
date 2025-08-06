c00c4fdbdb75090a16fe0bfe68cd1a7b2ac430fa
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2025-08-06 23:29:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+// #include <vector>
+#include <string>
+
+using namespace std;
+
+string str;
+
+
+int main(void) {
+
+    cin>>str;
+
+    cout<<str.length();
+
+    return 0;
+}

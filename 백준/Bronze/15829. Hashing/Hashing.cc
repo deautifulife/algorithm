ab0ebf489abc1853939073f43abf8f504dcb1ee8
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2025-08-13 23:30:38 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -22,11 +22,11 @@ int main() {
     int r = 31;
     long long power = 1;
     for(int i=0; i<str.length(); i++){
-        ret += (str[i]-'a' +1) * power;
+        ret += ((str[i]-'a' +1) * power) % M; // 누적에도 mod M을 안해줘서 50점만 나왔다고한다.
         power = (power * r) % M;
     }
 
-    cout<<ret;
+    cout<<ret % M;
 
 
     return 0; 

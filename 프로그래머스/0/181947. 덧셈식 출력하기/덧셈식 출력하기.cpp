9ddf611d7a39a519ee3a2b05a8cd9a5b2f298983
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2025-12-02 22:49:56 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 2.72 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    int res = a+b;
+    cout<<a<<" + "<<b<<" = "<<res;
+    return 0;
+}

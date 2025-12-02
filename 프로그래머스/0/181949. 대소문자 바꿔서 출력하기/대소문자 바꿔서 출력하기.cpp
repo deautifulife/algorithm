39d3137e20bcd15f163d343ab93a2d4c89bfb6d4
DongHyeon Kim <128145327+deautifulife@users.noreply.github.com>
2025-12-02 22:44:09 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 3.09 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(int i=0; i<str.length(); i++){
+        if(str[i] >= 'a')
+            str[i] = str[i] - 32;
+        else
+            str[i] = str[i] + 32;
+    }
+    cout<<str;
+    return 0;
+}
